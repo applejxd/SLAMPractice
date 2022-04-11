d1443b1a7e0618fc4e75fb7050f859c84ed12ffd
applejxd <9639592+applejxd@users.noreply.github.com>
2022-04-12 08:01:35 +0900

    add plotting function

--- src/g2o_sample.cpp
+++ src/g2o_sample.cpp
@@ -2,57 +2,57 @@
  * from https://github.com/gaoxiang12/slambook2
  */
 #include <iostream>
-#include <g2o/core/g2o_core_api.h>
 #include <g2o/core/base_vertex.h>
 #include <g2o/core/base_unary_edge.h>
 #include <g2o/core/block_solver.h>
-#include <g2o/core/optimization_algorithm_levenberg.h>
 #include <g2o/core/optimization_algorithm_gauss_newton.h>
-#include <g2o/core/optimization_algorithm_dogleg.h>
 #include <g2o/solvers/dense/linear_solver_dense.h>
 #include <Eigen/Core>
 #include <opencv2/core/core.hpp>
 #include <cmath>
 #include <chrono>
+#include <matplotlibcpp.h>
 
 using namespace std;
 
-// 曲线模型的顶点，模板参数：优化变量维度和数据类型
+// 頂点：最適化パラメータの次元・型(EstimateType)
 class CurveFittingVertex : public g2o::BaseVertex<3, Eigen::Vector3d> {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-    // 重置
+    // リセット処理
     void setToOriginImpl() override {
         _estimate << 0, 0, 0;
     }
 
-    // 更新
+    // 更新処理
     void oplusImpl(const double *update) override {
         _estimate += Eigen::Vector3d(update);
     }
 
-    // 存盘和读盘：留空
-    bool read(istream &in) override {}
+    // ダミー関数
+    bool read(istream &in) override { return true; }
 
-    bool write(ostream &out) const override {}
+    // ダミー関数
+    bool write(ostream &out) const override { return true; }
 };
 
-// 误差模型 模板参数：观测值维度，类型，连接顶点类型
+// 辺：接続する頂点の数(EdgeDimension)・測定の型(Measurement)・頂点の型(VertexXiType)
 class CurveFittingEdge : public g2o::BaseUnaryEdge<1, double, CurveFittingVertex> {
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
     explicit CurveFittingEdge(double x) : BaseUnaryEdge(), _x(x) {}
 
-    // 计算曲线模型误差
+    // 残差の定義
     void computeError() override {
         const auto *v = dynamic_cast<const CurveFittingVertex *> (_vertices[0]);
-        const Eigen::Vector3d abc = v->estimate();
-        _error(0, 0) = _measurement - std::exp(abc(0, 0) * _x * _x + abc(1, 0) * _x + abc(2, 0));
+        // 推定したパラメータ
+        const Eigen::Vector3d abc_est = v->estimate();
+        _error(0, 0) = _measurement - std::exp(abc_est[0] * _x * _x + abc_est[1] * _x + abc_est[2]);
     }
 
-    // 计算雅可比矩阵
+    // ヤコビ行列（のマイナス）
     void linearizeOplus() override {
         const auto *v = dynamic_cast<const CurveFittingVertex *> (_vertices[0]);
         const Eigen::Vector3d abc = v->estimate();
@@ -62,68 +62,119 @@ public:
         _jacobianOplusXi[2] = -y;
     }
 
-    bool read(istream &in) override {}
+    // ダミー関数
+    bool read(istream &in) override { return true; }
 
-    bool write(ostream &out) const override {}
+    // ダミー関数
+    bool write(ostream &out) const override { return true; }
 
 public:
     double _x;  // x 值， y 值为 _measurement
 };
 
-int main(int argc, char **argv) {
-    double ar = 1.0, br = 2.0, cr = 1.0;         // 真实参数值
-    double ae = 2.0, be = -1.0, ce = 5.0;        // 估计参数值
-    int N = 100;                                 // 数据点
-    double w_sigma = 1.0;                        // 噪声Sigma值
-    double inv_sigma = 1.0 / w_sigma;
-    cv::RNG rng;                                 // OpenCV随机数产生器
-
-    vector<double> x_data, y_data;      // 数据
-    for (int i = 0; i < N; i++) {
-        double x = i / 100.0;
-        x_data.push_back(x);
-        y_data.push_back(exp(ar * x * x + br * x + cr) + rng.gaussian(w_sigma * w_sigma));
+int main() {
+    // サンプリング数
+    uint16_t N = 100;
+    // ガウシアンノイズの標準偏差
+    double w_sigma = 1.0;
+    // サンプリングデータ
+    vector<double> x_data, y_true_data, y_obs_data;
+    {
+        // パラメータの正解
+        double ar = 1.0, br = 2.0, cr = 1.0;
+        // 乱数生成器
+        cv::RNG rng;
+        // データ生成
+        for (uint16_t i = 0; i < N; i++) {
+            double x = i / 100.0;
+            x_data.push_back(x);
+            y_true_data.push_back(exp(ar * x * x + br * x + cr));
+            y_obs_data.push_back(exp(ar * x * x + br * x + cr) + rng.gaussian(w_sigma * w_sigma));
+        }
     }
 
-    // 构建图优化，先设定g2o
-    typedef g2o::BlockSolver<g2o::BlockSolverTraits<3, 1>> BlockSolverType;  // 每个误差项优化变量维度为3，误差值维度为1
-    typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType> LinearSolverType; // 线性求解器类型
+    g2o::SparseOptimizer solver;
+    {
+        // 状態変数は3つ(PoseDim)・観測値は1つ(LandmarkDim)
+        typedef g2o::BlockSolver<g2o::BlockSolverTraits<3, 1>> BlockSolverType;
+        // 線形ソルバ設定
+        typedef g2o::LinearSolverDense<BlockSolverType::PoseMatrixType> LinearSolverType;
 
-    // 梯度下降方法，可以从GN, LM, DogLeg 中选
-    auto solver = new g2o::OptimizationAlgorithmGaussNewton(
-            g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
-    g2o::SparseOptimizer optimizer;     // 图模型
-    optimizer.setAlgorithm(solver);   // 设置求解器
-    optimizer.setVerbose(true);       // 打开调试输出
+        // Gauss-Newton 法での最適化アルゴリズム
+        auto opt_algorithm = new g2o::OptimizationAlgorithmGaussNewton(
+                g2o::make_unique<BlockSolverType>(g2o::make_unique<LinearSolverType>()));
+        // 最適化アルゴリズム設定
+        solver.setAlgorithm(opt_algorithm);
+        // デバッグ情報有効化
+        solver.setVerbose(true);
+    }
 
-    // 往图中增加顶点
+    // 頂点
     auto *v = new CurveFittingVertex();
-    v->setEstimate(Eigen::Vector3d(ae, be, ce));
-    v->setId(0);
-    optimizer.addVertex(v);
-
-    // 往图中增加边
-    for (int i = 0; i < N; i++) {
-        auto *edge = new CurveFittingEdge(x_data[i]);
-        edge->setId(i);
-        edge->setVertex(0, v);                // 设置连接的顶点
-        edge->setMeasurement(y_data[i]);      // 观测数值
-        edge->setInformation(Eigen::Matrix<double, 1, 1>::Identity() * 1 / (w_sigma * w_sigma)); // 信息矩阵：协方差矩阵之逆
-        optimizer.addEdge(edge);
+    {
+        // 推定したパラメータ
+        double ae = 2.0, be = -1.0, ce = 5.0;
+        // 頂点に推定値を設定
+        v->setEstimate(Eigen::Vector3d(ae, be, ce));
+        // 頂点の ID 設定
+        v->setId(0);
+        // ソルバに頂点登録
+        solver.addVertex(v);
     }
 
-    // 执行优化
-    cout << "start optimization" << endl;
-    chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
-    optimizer.initializeOptimization();
-    optimizer.optimize(10);
-    chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
-    chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
-    cout << "solve time cost = " << time_used.count() << " seconds. " << endl;
+    // 辺登録
+    for (uint16_t i = 0; i < N; i++) {
+        auto *edge = new CurveFittingEdge(x_data[i]);
+        // 辺に ID 設定
+        edge->setId(i);
+        // 辺の 0 番目の頂点が v
+        edge->setVertex(0, v);
+        // 辺に測定値登録
+        edge->setMeasurement(y_obs_data[i]);
+        // 情報行列（誤差共分散行列の逆行列）
+        edge->setInformation(Eigen::Matrix<double, 1, 1>::Identity() * 1 / (w_sigma * w_sigma));
+        // ソルバに辺登録
+        solver.addEdge(edge);
+    }
 
-    // 输出优化值
-    Eigen::Vector3d abc_estimate = v->estimate();
-    cout << "estimated model: " << abc_estimate.transpose() << endl;
+    {
+        cout << "start optimization" << endl;
+        chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
+        solver.initializeOptimization();
+        // 繰り返し回数10回
+        solver.optimize(10);
+        chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
+        chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+        cout << "solve time cost = " << time_used.count() << " seconds. " << endl;
+    }
+
+    // 推定結果
+    Eigen::Vector3d abc_est = v->estimate();
+    cout << "estimated model: " << abc_est.transpose() << endl;
+
+
+    {
+        namespace plt = matplotlibcpp;
+
+        // 散布図のフォーマット
+        map<string, string> scatter_map;
+        scatter_map["c"] = "gray";
+        // 測定データ
+        plt::scatter(x_data, y_obs_data, 2.0, scatter_map);
+
+        // 答え
+        // cf. https://matplotlib.org/stable/tutorials/colors/colors.html
+        plt::plot(x_data, y_true_data, "tab:blue");
+
+        // 推定結果
+        vector<double> y_est_data;
+        for (const auto &x: x_data) {
+            y_est_data.push_back(exp(abc_est[0] * x * x + abc_est[1] * x + abc_est[2]));
+        }
+        plt::plot(x_data, y_est_data, "tab:orange");
+
+        plt::show();
+    }
 
     return 0;
 }
